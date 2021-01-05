2081d394385f27220ed500551a70b422a33af6e0
jack wu <cnly1987@gmail.com>
2021-01-05 21:33:14 +0800

    ...

--- libs/mg/query.py
+++ libs/mg/query.py
@@ -6,7 +6,6 @@ from django.conf import settings
 def mgdb():
     try:
         client = pymongo.MongoClient(settings.MONGO_BROKER_URL)
-        # client.admin.authenticate('cnly1987', 'wupeng1987', mechanism = 'SCRAM-SHA-1', source='nqmonitor')
         return client
     except Exception as e:
         client.close()
